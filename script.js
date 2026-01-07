df7b63f9d9d93d8c3e0dfd59eee4edeb0d2fba37
ごまみそ <noritomo3.m.m@gmail.com>
2026-01-08 01:35:23 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const GAS_API_URL = "https://script.google.com/macros/s/AKfycbwUAlllOM9Tncn732dvIc9j4si4d4PJDUgov8nT9tb9-U3kxaS5y8J8x8FjA8Lf-SxbNg/exec";
+const GAS_API_URL = "https://script.google.com/macros/s/AKfycbz35ARxxw6AmCo4ZJ_EQA0NVRmSqxMp-1fCC7GtUU-MY8zjCIJ1_RkonxxspQ-uoLCpow/exec";
 
 
 let DATA = {};
