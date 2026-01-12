2257f0d746a2708ac06ae0ecd39b70b93d1bb830
ごまみそ <noritomo3.m.m@gmail.com>
2026-01-12 16:08:56 +0900

    Update app.js

--- app.js
+++ app.js
@@ -261,7 +261,7 @@ function renderTile() {
   }).join('');
 }
 
-function getFitSpan(text, baseSize, limitWidth = 60) {
+function getFitSpan(text, baseSize, limitWidth = 55) {
   let estimatedWidth = 0;
   for (let char of String(text)) {
     // 半角は0.6倍、全角は1倍で幅を概算
